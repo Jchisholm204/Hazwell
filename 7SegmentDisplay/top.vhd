89193be2254f59dd15d58e862d985f40f5affe71
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2023-02-27 22:26:33 -0500

    finally starting to show resemblance of working

--- 7SegmentDisplay/top.vhd
+++ 7SegmentDisplay/top.vhd
@@ -4,7 +4,7 @@ use ieee.numeric_std.all;
 
 entity top is
 	port(
-		s0, s1, s2 : IN STD_LOGIC;
+		dipSwitches : in std_logic_vector(7 downto 0);
 		ledr0 : OUT STD_LOGIC;
 		hex0 : out std_logic_vector(6 downto 0)
 
@@ -12,16 +12,21 @@ entity top is
 end top;
 
 architecture main of top is
-signal num : unsigned(7 downto 0);
+	signal number : std_logic_vector(7 downto 0);
 begin
 
-	main : process(num) begin
-	if (s0 = '0') then
-	hex0 <= "1000000"; -- 0 is on
-	elsif s0 = '1' then
-	hex0 <= "1111001"; -- 6 5 4 3 2 1 0
-	end if;
-	end process main;
+	number <= dipSwitches;
+
+	process(number) is
+	begin
+	
+		if number = "00000000" then
+			hex0 <= "1000000"; -- 0 is on
+		elsif number = "00000001" then
+			hex0 <= "1111001"; -- 6 5 4 3 2 1 0
+		end if;
+		
+	end process;
 
 				
 end architecture;
