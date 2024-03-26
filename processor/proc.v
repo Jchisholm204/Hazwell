8def300f2bafe8ca68ac1acb57d743e793d27da4
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2024-03-25 22:40:07 -0400

    wip

--- processor/proc.v
+++ processor/proc.v
@@ -11,10 +11,11 @@ output wire [31:0] oMemData, oMemAddr;
 output wire oMemWrite, oMemRead;
 
 // Memory Data
-wire [31:0] IR_out, MDR_out;
-wire IR_en, MDR_en;
+wire [31:0] IR_out, MDR_out, MAR_in;
+wire IR_en, MDR_en, MAR_en;
 REG32 IR (.iClk(iClk), .nRst(nRst), .iEn(IR_en), .iD(iMemData), .oQ(IR_out));
 REG32 MDR (.iClk(iClk), .nRst(nRst), .iEn(MDR_en), .iD(iMemData), .oQ(MDR_out));
+REG32 MAR (.iClk(iClk), .nRst(nRst), .iEn(MAR_en), .iD(MAR_in), .oQ(oMemAddr));
 
 
 // Register File
@@ -24,14 +25,12 @@ wire RF_Write;
 
 registers regs( .iClk(iClk), .nRst(nRst), .iWrite(RF_Write), .iAddrA(RF_AddrA), .iAddrB(RF_AddrB), .iAddrC(RF_AddrC), .oRegA(RF_RA), .oRegB(RF_RB), .iRegC(RF_RC));
 
+// Registers to hold RF Outputs
 wire [31:0] RA_out, RB_out;
-wire RA_en, RB_en;
 
-REG32 RA (.iClk(iClk), .nRst(RA_en), .iD(RF_RA), .oQ(RA_out));
-REG32 RB (.iClk(iClk), .nRst(RB_en), .iD(RF_RB), .oQ(RB_out));
+REG32 RA (.iClk(iClk), .nRst(1'b1), .iD(RF_RA), .oQ(RA_out));
+REG32 RB (.iClk(iClk), .nRst(1'b1), .iD(RF_RB), .oQ(RB_out));
 
-assign RA_en = 1'b1;
-assign RB_en = 1'b1;
 
 // ALU
 wire [5:0] ALU_op;
@@ -40,6 +39,17 @@ wire [31:0] ALU_RA, ALU_RB, ALU_RC;
 
 ALU alu(.OP(ALU_op), .oZero(ALU_zero), .oNeg(ALU_neg), .iRA(ALU_RA), .iRB(ALU_RB), .oRC(ALU_RC));
 
+// ALU output register
+wire [31:0] ALU_out;
+REG32 RALU (.iClk(iClk), .nRst(1'b1), .iD(ALU_RC), .oQ(ALU_out));
+
+
+// Datapath Control Signals
+wire MAR_Select, ALUsrcB_Select;
+wire [1:0] PC_Select, RFC_Select;
+
+
+// Memory Data Out
 
 
 
