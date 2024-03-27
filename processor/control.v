8afd00706a1be57e233182c54e9a9884203558de
Jacob Chisholm <jacobchisholm1010@gmail.com>
2024-03-27 18:31:54 -0400

    Create control.v

---
+++ processor/control.v
@@ -0,0 +1,12 @@
+module control(
+    iClk, nRst, iPC,
+    PC_en, IR_en, MDR_en, MAR_en,
+    RF_Write,
+    ALU_op,
+    MAR_Select, ALUB_Select, AddrC_Select, PCA_Select,
+    PC_Select, C_Select
+);
+
+
+
+endmodule
